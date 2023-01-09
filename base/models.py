eeb41afa2b252ce425c48d505eee2a0b3a55d771
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-09 14:14:57 +0000

    initial commit

---
+++ base/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+# from django.contrib.auth.models import User
+
+
+# class UtubeUser (models.Model):
+#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     phone_number = models.IntegerField()
+
+#     def __str__(self):
+#         self.phone_number
---
+++ videos/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+# from base.models import UtubeUser
+
+import uuid
+
+
+class Video(models.Model):
+    PROGRAMMING = 'PR'
+    COMMEDY = 'CM'
+    DOCUMENTRY = 'DC'
+    FOOTBALL = 'FT'
+    MUSIC = 'MC'
+    GAMING = 'GM'
+    OTHERS = 'OT'
+
+    CATEGORY_CHOICES = [
+        (PROGRAMMING, 'Computer Programming'),
+        (COMMEDY, 'Commedy'),
+        (DOCUMENTRY, 'Documentry'),
+        (FOOTBALL, 'Football'),
+        (MUSIC, 'Music'),
+        (GAMING, 'Gaming'),
+        (OTHERS, 'Others')]
+
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200, unique=True)
+    uploaded_date = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(upload_to='photo/%Y/%m/%d/')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.FileField(upload_to='vidoes/%Y/%m/%d/')
+    total_views = models.IntegerField(default=0)
+    category = models.CharField(max_length=3, choices=CATEGORY_CHOICES)
+    description = models.TextField(
+        default='New Video has been added', blank=True)
+    total_likes = models.IntegerField(default=0)
+    duration = models.DurationField()
+
+    def __str__(self):
+        return self.title
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=300, blank=False)
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    channel_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(default='New channel created', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Comments(models.Model):
+    comment_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_comment = models.ForeignKey('Video', on_delete=models.CASCADE)
+    date_of_comment = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=False)
