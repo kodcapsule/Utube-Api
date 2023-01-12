e62a9cbb5fb1db89f1a7e5ed3ad4c99f9eed011d
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-12 22:42:45 +0000

    added authentication to function based view

--- api/views.py
+++ api/views.py
@@ -4,19 +4,23 @@ from .serializers import Videoserializer
 from django.http import JsonResponse
 
 from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.authentication import BaseAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 # FUNCTION BASED VIEWS
 
 @api_view(['GET'])
+@permission_classes((IsAuthenticated,))
 def VideosList(request):
     print(request.method)
-    if request.method == 'GET':
-        videos = Video.objects.all()
-        serializer = Videoserializer(videos, many=True)
-        return Response(serializer.data)
+    if (IsAuthenticated):
+        if request.method == 'GET':
+            videos = Video.objects.all()
+            serializer = Videoserializer(videos, many=True)
+            return Response(serializer.data)
 
 
 def index(request):
